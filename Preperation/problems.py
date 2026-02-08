4786331e6f3ddf9d779848c1a5058381bb47f0a5
deni <mariadeniston111@gmail.com>
2026-02-08 20:07:36 +0530

    notes updated

--- Preperation/problems.py
+++ Preperation/problems.py
@@ -1,2 +1,19 @@
 for number in [12,2,34,2,3]:
     print(number)
+
+def isPalindrome(num):
+    
+    temp = num
+    rev = 0
+    
+    while num > 0 :
+        last_digit = num % 10
+        rev = (rev * 10) + last_digit
+        num = num // 10
+    if temp == rev:
+        return "palindrome"
+    else:
+        return "not palindrome"
+        
+print(isPalindrome(123))
+print(isPalindrome(323))
